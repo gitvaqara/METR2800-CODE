0d9cff3ac88a04159826c61fcc104dc35ef25fd8
Ryan <144753839+3541431777@users.noreply.github.com>
2024-05-20 13:42:11 +1000

    attempts to cut down time, ill be back at 4

--- Main_Sketch.ino
+++ Main_Sketch.ino
@@ -8,10 +8,9 @@
  */
 
 // including the necessary libraries
-#include <SoftwareSerial.h>
 #include <Stepper.h>
 #include <Servo.h>
-#include <DFRobot_LIDAR07.h>
+//#include <DFRobot_LIDAR07.h>
 
 
 // initialising DC MOTOR driver pins
@@ -38,7 +37,7 @@ const int OUT_RFRONT = 10;
 
 // initialising STEPPER driver pins
 // Intialising the amount of Steps per Revolution
-const int StepsPerRev = 200;
+const int StepsPerRev = 500;
 
 // STEPPER Motor - Left
 const int SLdirection = 23;
@@ -46,7 +45,7 @@ const int SLstep = 22;
 const int SLMS3 = 19;
 
 // STEPPER Motor - RIGHT
-const int SRdirection = 24;
+const int SRdirection = 33;
 const int SRstep = 25;
 const int SRMS3 = 28;
 
@@ -95,7 +94,7 @@ void setup() {
   MAIN.attach(SERVO_MAIN_PIN);
   BLOCK.attach(SERVO_BLOCK_PIN);
 
-  // Stepper Configurations
+  /* -------------- Stepper Configurations -------------- */
   // Declare MS3 pins as Outputs
   pinMode(SLMS3, OUTPUT);
   pinMode(SRMS3, OUTPUT);
@@ -103,6 +102,12 @@ void setup() {
   // Set MS3 pins to HIGH
   digitalWrite(SLMS3, HIGH);
   digitalWrite(SRMS3, HIGH);
+
+  // Set the Stepper Speeds
+  stepperLEFT.setSpeed(200);
+  stepperRIGHT.setSpeed(200);
+  /* ----------- End of Stepper Configurations --------- */
+
 
   // Setting up the TOF IR SENSOR
   
@@ -114,43 +119,202 @@ void setup() {
   /* ----- Code to Pickup a Ball ---------
      -------------------------------------
      ------ Run with the following -------
-     Distance from Base to Stepup: 16.5cm
+      Distance from Base to Stepup: 14cm
      Scoop lined up with the highest ball
       Scoop rotation position not needed
      -------------------------------------
      -------------------------------------
   */
-  move_mainservo_pos30(); // Moves the Servo to the starting position 
-  delay(5000); 
-  move_mainservo_pos168(); // Moves the Servo to a flat position
-  delay(6000); // Can reduce slightly to 5000
-  forward_setup();
-  go();
-  delay(2000);
-  stop();
-  move_mainservo_pos160(); // Tilts the servo up slightly to pick up the ball
-  delay(2500);
-  back_setup();
-  go();
-  delay(7500);
-  stop();
-  rotate_anticlockwise_setup();
-  go();
-  delay(10600); // 5300 for 90degree rotation; 10600 for 180degree rotation
-  stop();
-  delay(1000);
-  move_mainservo_pos120();
+  //delete these, for resetting only
+  mainservo_angle(40);
   delay(5000);
-  // 
-  back_setup();
-  go();
-  delay(1000); // Experiment with this to see how far back to go in order to drop ball in
 
+  mainfast_angle(177); //get this moving asap, no delay, concurrent
+  
+  pickup_ball1();
+  driveto_ball2();
+  pickup_ball2();
+  driveto_ball3();
+  pickup_ball3();
+  dropball_123();
+  disturb_ball();
 
 }
 
 void loop() {
   // If not using TOF IR OR Yellow IR, loop not being used
+}
+
+
+/* -----------------------------------
+ * -----------------------------------
+ *        Pickup Ball Functions
+ * -----------------------------------
+ * -----------------------------------
+ */
+
+void pickup_ball1() {
+ /* Servo Setup Parameters:
+    Start Position = 20degrees
+    Straight Up = 100degrees
+    Tilt up to collect Ball = 150degrees
+    Angled Position to collect Highest Ball = 152degrees
+    Parallel = 160degrees
+    Drop Ball = 175degrees
+  */
+  blockservo_open(); // Open up the Blocking Servo
+  //mainfast_angle(20); // Moves the Servo to the starting position
+  mainfast_angle(170); // Moves the Servo to the angled position
+  delay(1500);
+  mainfast_angle(177);
+  delay(2000);
+  speed(255);
+  delay(750);
+  stop();
+  mainfast_angle(120); // Tilts the servo up slightly to pick up the ball
+  delay(500);
+  //blockservo_close();
+
+}
+
+void driveto_ball2() {
+  // Drives to the 2nd Ball
+  speedtime(-255,6500);
+  //back_setup();
+  //go();
+  //delay(6500);
+  //stop();
+  rotate_anticlockwise_setup(); // Rotates 
+  stepper_midheight();
+  go();
+  delay(11300); // 5650 for 90degree rotation; 11300 for 180degree rotation
+  stop();
+  strafe_left_setup();
+  go();
+  delay(400);
+  stop();
+}
+
+void pickup_ball2() {
+  mainfast_angle(170);
+  delay(500);
+  mainfast_angle(177); // Moves the Servo to the angled position
+  delay(1000); 
+  forward_setup();
+  go();
+  delay(1500);
+  stop();
+  delay(200);
+  blockservo_open(); // Open up the Blocking Servo
+  mainfast_angle(120); // Tilts the servo up slightly to pick up the ball
+  delay(600);
+  blockservo_close();
+  //delay(200);
+}
+
+/* ---------------------------- Code to Drop Ball 1 & 2 ----------------------- */
+/*
+void dropball_12() {
+  back_setup();
+  go();
+  delay(3000);
+  stop();
+  rotate_clockwise_setup();
+  go();
+  delay(5650);
+  stop();
+  back_setup();
+  go();
+  delay(2100);
+  stop();
+  strafe_right_setup();
+  go();
+  delay(800);
+  stop();
+  stepper_any(10000, 'd');
+  delay(500);
+  mainservo_angle(170);
+  delay(1000);
+  blockservo_open();
+  delay(500);
+}
+*/
+/* ---------------------- End of Code to Drop Ball 1 & 2 ---------------------- */
+
+void driveto_ball3() {
+  stepper_baseheight();
+  strafe_right_setup();
+  go();
+  delay(5500);
+  stop();
+  back_setup();
+  mainfast_angle(177);
+  go();
+  delay(1300);
+  stop();
+}
+
+void pickup_ball3() {
+  //delay(1000);
+  //forward_setup();
+  //go();
+  //delay(150);
+  //stop();
+  //mainservo_angle(165); // Moves the Servo to the angled position
+  mainfast_angle(177);
+  delay(200); 
+  forward_setup();
+  go();
+  delay(1400);
+  stop();
+  delay(200);
+  mainfast_angle(100); // Tilts the servo up slightly to pick up the ball
+  delay(100);
+  blockservo_open(); // Open up the Blocking Servo
+  delay(200);
+  // blockservo_close();
+  // delay(200);
+}
+
+void dropball_123() {
+  back_setup();
+  go();
+  delay(1500);
+  stop();
+  strafe_left_setup();
+  go();
+  delay(5500);
+  back_setup();
+  go();
+  delay(2800);
+  stop();
+  rotate_clockwise_setup();
+  go();
+  delay(5650);
+  stop();
+  back_setup();
+  go();
+  delay(2300);
+  stop();
+  strafe_right_setup();
+  go();
+  delay(50);
+  stop();
+  mainservo_angle(170);
+  delay(500);
+  blockservo_open();
+}
+
+void disturb_ball() {
+  mainservo_angle(152);
+  rotate_clockwise_setup();
+  go();
+  delay(3000);
+  stop();
+  forward_setup();
+  go();
+  delay(3000);
+  stop();
 }
 
 /* -----------------------------------
@@ -160,13 +324,39 @@ void loop() {
  * -----------------------------------
  */
 
+
 void go() {
   // Set motors to move forward at speed 200
-  analogWrite(SPEED_LFRONT, 200); // Left Front Motor speed
-  analogWrite(SPEED_RFRONT, 200); // Right Front Motor speed
-  analogWrite(SPEED_LBACK, 200);  // Left Back Motor speed
-  analogWrite(SPEED_RBACK, 200);  // Right Back Motor speed
+  analogWrite(SPEED_LFRONT, 250); // Left Front Motor speed
+  analogWrite(SPEED_RFRONT, 250); // Right Front Motor speed
+  analogWrite(SPEED_LBACK, 250);  // Left Back Motor speed
+  analogWrite(SPEED_RBACK, 250);  // Right Back Motor speed
 }
+
+void speedtime(int sp, int time){
+  speed(sp);
+  delay(time);
+  speed(0);
+}
+
+void speed(int sp){
+  if (sp >= 0){
+    forward_setup();
+  } else {
+    back_setup();
+  }
+
+  if (sp <= 256) {
+    if (sp < 0) {
+      sp=-sp;
+    }
+    analogWrite(SPEED_LFRONT, sp); // Left Front Motor speed
+    analogWrite(SPEED_RFRONT, sp); // Right Front Motor speed
+    analogWrite(SPEED_LBACK, sp);  // Left Back Motor speed
+    analogWrite(SPEED_RBACK, sp);
+  }
+}
+
 
 void stop() {
   // Stop all motors
@@ -268,73 +458,94 @@ void rotate_anticlockwise_setup() {
  * -----------------------------------
  */
 
+// void move_servo(int pos){
+//   if (pos <= 240 && pos >= 25){
+//     MAIN.write(pos);
+//     delay(2000);
+//     if (pos <= 120) {
+//       MAIN.write(pos + 1);
+//     } else {
+//       MAIN.write(pos - 1);
+//     }
+//     delay(500);
+//   }
+// }
+void mainfast_angle(int angle){
+  MAIN.write(angle);
+}
 
-void move_mainservo_pos30() { 
-  // Starting Position for the Arm
-  MAIN.write(30);
+void mainservo_angle(int angle) {
+  MAIN.write(angle-10);
   delay(2000);
-  MAIN.write(0);
+  MAIN.write(angle);
+  delay(2000);
+  MAIN.write(angle);
+}
+
+/* -----------------------------------
+ * -----------------------------------
+ *       Arm Height Functions
+ * -----------------------------------
+ * -----------------------------------
+ */
+
+
+void stepper_baseheight() {
+  // Moving the Stepper to the base height
+  for (int i = 0; i < 12000; i++){
+    stepperRIGHT.step(1);
+    stepperLEFT.step(1);
+  }
   delay(500);
-  MAIN.write(30);
-  delay(2000);
 }
 
-void move_mainservo_pos160() {
-  // Pickup positon to tilt ball into servo
-  MAIN.write(160);
-  delay(2500);
-  MAIN.write(160);
-  delay(2500);
-  MAIN.write(160);
-  delay(2500);
+void stepper_midheight() {
+  // Moving the Stepper to the Middle Ball height 
+  for (int i = 0; i < 6000; i++){
+    stepperRIGHT.step(1);
+    stepperLEFT.step(1);
+  }
+  delay(500);
 }
 
-void move_mainservo_pos168() {
-  // Position parallel to floor
-  MAIN.write(168);
-  delay(2000);
-  MAIN.write(140);
-  delay(2000);
-  MAIN.write(168);
-  delay(2000);
-  MAIN.write(168);
-  delay(2000);
+void stepper_any(int steps, char direction) {
+  // Takes any amount of Steps and runs it
+  for (int i = 0; i < steps; i++){
+    if (direction == 'd') { // Moves the stepper down
+      stepperRIGHT.step(1);
+      stepperLEFT.step(1);
+    }
+    else if (direction == 'u') { // Moves the stepper up
+      stepperRIGHT.step(-1);
+      stepperLEFT.step(-1);
+    }
+  }
 }
 
-void move_mainservo_pos120() {
-  // Position required to move servo down
-  // Nearly in line with the pillars
-  MAIN.write(120);
-  delay(2500);
-  MAIN.write(120);
-  delay(2500);
-  MAIN.write(120);
-  delay(2500);
-}
 
-void move_mainservo_drop() {
-  // Lowest position that the servo can go to drop the ball down
-  MAIN.write(240);
-  delay(2000);
-  MAIN.write(240);
-  delay(2000);
-  MAIN.write(240);
-  delay(2000);
-}
+/* -----------------------------------
+ * -----------------------------------
+ *         Tube Functionality
+ * -----------------------------------
+ * -----------------------------------
+ */
 
-void move_blockservo_open() {
+
+void blockservo_open() {
   // Open positon to let balls into tube
   // Havent plugged these into the PCB, need to test this
-  BLOCK.write(0);
+  BLOCK.write(80);
+}
+
+void blockservo_close() {
+  // Closed position to stop balls from rolling out
+  // Havent plugged these into the PCB, need to experiment positions
+  BLOCK.write(140);
   delay(500);
 }
 
-void move_blockservo_close() {
-  // Closed position to stop balls from rolling out
-  // Havent plugged these into the PCB, need to experiment positions
-  BLOCK.write(90);
-  delay(500);
-}
+
+
 
 
 
